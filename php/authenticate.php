315a37527e42ab244d5cebb9b9400c35b7d12509
bote795 <bote795@vm-0.bote795.koding.kd.io>
2014-06-24 18:30:20 +0000

    add error catching and session

--- php/authenticate.php
+++ php/authenticate.php
@@ -1,11 +1,12 @@
 <?php
 
 include 'connect.php';
-
 include 'PasswordHash.php';
+
 $text = array();
 $action = array();
 $action['result'] = null;
+
 $username = mysql_real_escape_string($_POST['username']); 
 $password =mysql_real_escape_string($_POST['password']);
   if(empty($username))
@@ -18,36 +19,32 @@ $password =mysql_real_escape_string($_POST['password']);
     $action['result'] = 'error';
     array_push($text,'You forgot your password');
   }
-$action['text'] = $text;
-
- 	echo $action['result'];
+  $action['text'] = $text;
 
 	if($action['result'] != 'error')
-   {
-     echo "went in";
-     
+   {  
       $result = mysql_query("SELECT * FROM info WHERE username ='$username'") or die(mysql_error());
       $numRows = mysql_num_rows($result);
-  
         if($numRows == 1) 
         {
           $row = mysql_fetch_array( $result );
-          
-            $validate = validate_password($password, $row['password']);      
-             if($validate == 1)
-             {
-                echo "sucessful login";
-             }
-            else 
-            {
-              echo "unsucessful login";
-            }
+          $validate = validate_password($password, $row['password']);      
+          if($validate == 1)
+          {
+            echo "sucessful login";
+            session_start();
+            $_SESSION['username'] = 'true';
+          }
+          else 
+          {
+            echo "unsucessful login";
+          }
         }//close row if statment
      
-   }
-   else
-   {
-     show_errors($action);
-   }
+  }
+  else
+  {
+    echo show_errors($action);
+  }
 
  ?>
--- php/connect.php
+++ php/connect.php
@@ -10,20 +10,17 @@ echo "Connected to MySQL<br>";
 $selected = mysql_select_db("school",$dbhandle) or die('could not connect to school db' . mysql_error());
 
 //cleanup the errors
+
 function show_errors($action){
-
 	$error = false;
-
-	if(!empty($action['result'])){
-	
+	if(!empty($action['result']))
+    {
 		$error = "<ul class=\"alert $action[result]\">"."\n";
-
 		if(is_array($action['text'])){	
 			//loop out each error
 			foreach($action['text'] as $text){
 				$error .= "<li><p>$text</p></li>"."\n";
 			}	
-		
 		}else{
 			//single error
 			$error .= "<li><p>$action[text]</p></li>";
